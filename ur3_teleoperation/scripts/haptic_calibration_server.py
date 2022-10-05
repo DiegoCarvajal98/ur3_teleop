84cc52713ca193f7c5fc72df1b339e9f2e10dd7b
Diego Carvajal <diego98cs@gmail.com>
2022-10-04 20:26:21 -0500

    Added haptic tool calibration server node

---
+++ ur3_teleoperation/scripts/haptic_calibration_server.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+import rospy
+from std_srvs.srv import Empty, EmptyResponse
+from std_msgs.msg import Float32
+
+class ToolCalibration():
+    def __init__(self) -> None:
+        self.tool_angle_subscriber = rospy.Subscriber('tool_angle', Float32, self.toolAngleCallback)
+        self.server = rospy.Service('tool_calibration',Empty,self.handleToolCalibration)
+
+    def toolAngleCallback(self,msg) -> None:
+        self.tool_angle = msg.data
+
+    def handleToolCalibration(self,req) -> EmptyResponse:
+        rospy.loginfo("Set the tool to the minimum opening")
+
+        start_time = rospy.Time.now().secs
+
+        while(rospy.Time.now().secs - start_time < 5):
+            continue
+        
+        tool_min = self.tool_angle
+        rospy.set_param('/tool_min', tool_min)
+        rospy.loginfo('Haptic tool minimum opening set to {tool_min}')
+
+        rospy.loginfo("Set the tool to the maximum opening")
+
+        start_time = rospy.Time.now().secs
+
+        while(rospy.Time.now().secs - start_time < 5):
+            continue
+        
+        tool_max = self.tool_angle
+        rospy.set_param('/tool_max', tool_max)
+        rospy.loginfo('Haptic tool maximum opening set to {tool_max}')
+
+        return EmptyResponse()
+
+if __name__ == '__main__':
+    rospy.init_node('tool_calibration_server')
+    tool_calibration = ToolCalibration()
+    rospy.spin()
